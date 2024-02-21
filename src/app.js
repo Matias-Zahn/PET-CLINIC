46adc218c969bfbe33938f9a5a86f31a7307393b
Matias Zahn <zahnmatias14@gmail.com>
2024-02-21 20:54:30 -0300

    Implementacion de express rate limit

--- src/app.js
+++ src/app.js
@@ -3,15 +3,21 @@ import { AppError } from "./common/errors/appError.js";
 import { globalErrorHandle } from "./common/errors/error.controller.js";
 import { enableCors } from "./config/plugins/cors.js";
 import { router } from "./routes/index.js";
+import { limitRequest } from "./config/plugins/rateLimit.js";
 
 const app = express();
+const rateLimit = limitRequest(
+   1000,
+   60,
+   "Too many request from this IP, please try again in an hour!"
+);
 
 enableCors(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(rateLimit);
 
 //ROUTES <------
-
 app.use("/api/v1", router);
 
 app.all("*", (req, res, next) => {
---
+++ src/config/plugins/rateLimit.js
@@ -0,0 +1,9 @@
+import rateLimit from "express-rate-limit";
+
+export const limitRequest = (maxRequest, windowMinutes, message) => {
+   return rateLimit({
+      max: maxRequest,
+      windowMs: windowMinutes * 60 * 1000,
+      message: message,
+   });
+};
